cb1ddbc6cdafb0f2162505c701dc559135fb98d2
Fitri Ratna Dewi <fratnadewi@gmail.com>
2023-04-11 20:05:04 +0700

    Add script

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const ACTIVECLASS = "active";
+const IMAGES = document.querySelectorAll(".slider");
+
+IMAGES[0].classList.add(ACTIVECLASS);
+
+function removeActiveClass() {
+  const element = document.querySelector(`.${ACTIVECLASS}`);
+  if (element) {
+    element.classList.remove(ACTIVECLASS);
+  }
+}
+
+function addActiveClass($event) {
+  $event.stopPropagation();
+  removeActiveClass();
+  const target = $event.currentTarget;
+  target.classList.add(ACTIVECLASS);
+}
+
+IMAGES.forEach((image) => {
+  image.addEventListener("mouseover", addActiveClass);
+});
