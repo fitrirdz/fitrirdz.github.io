a69142a44c5b4e08c12507e41e33c89f139b5418
Fitri R Dewi <fitri@softwareseni.com>
2024-03-19 16:28:09 +0700

    Add responsive portfolio

---
+++ portfolio/assets/js/main.js
@@ -0,0 +1,28 @@
+// SHOW MENU
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
+// MENU SHOW
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+// MENU HIDDEN
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+// CHANGE BACKGROUND HEADER
+const bgHeader = () => {
+  const header = document.getElementById('header');
+
+  this.scrollY >= 50
+    ? header.classList.add('bg-header')
+    : header.classList.remove('bg-header');
+};
+window.addEventListener('scroll', bgHeader);
