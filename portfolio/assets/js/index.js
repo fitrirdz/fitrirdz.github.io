209765c4548633c6b269fac47ca77d8147cea67b
Fitri R Dewi <fitri@softwareseni.com>
2024-03-25 09:58:28 +0700

    Add work and testimonial sections

--- portfolio/assets/js/index.js
+++ portfolio/assets/js/index.js
@@ -20,3 +20,16 @@ modalClose.forEach((modalClose, i) => {
     });
   });
 });
+
+// SWIPER TESTIMONIAL
+const swiperTestimonial = new Swiper('.testimonial__swiper', {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true,
+  },
+});
