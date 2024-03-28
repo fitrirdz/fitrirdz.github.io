90ac88f6070a3d2720b8cc9b6959b325b4b87d96
Fitri R Dewi <fitri@softwareseni.com>
2024-03-28 16:42:57 +0700

    Add theme switcher

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -26,3 +26,38 @@ const bgHeader = () => {
     : header.classList.remove('bg-header');
 };
 window.addEventListener('scroll', bgHeader);
+
+// DARK LIGHT THEME
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'ri-sun-line' : 'ri-moon-line';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we apply the corresponding theme to the interface
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](
+    iconTheme
+  );
+}
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  // We save the theme and current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
