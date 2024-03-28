9f24d3774ebfcfb0b5cd9ed31d795f771831f554
Fitri R Dewi <fitri@softwareseni.com>
2024-03-28 15:49:40 +0700

    Add scroll up button

--- portfolio/assets/js/index.js
+++ portfolio/assets/js/index.js
@@ -33,3 +33,14 @@ const swiperTestimonial = new Swiper('.testimonial__swiper', {
     clickable: true,
   },
 });
+
+// SHOW SCROLL UP
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 350 viewport height, add the class
+  this.scrollY >= 350
+    ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll');
+};
+
+window.addEventListener('scroll', scrollUp);
